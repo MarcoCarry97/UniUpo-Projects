9bfbc99b06a9fb2c052ba9a409cbd790356066fa
Marco Carega <marcocarega.dev@gmail.com>
2022-11-09 15:54:52 +0100

    resystemed files

---
+++ NS/sir_test.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Nov  2 15:44:05 2022
+
+@author: Marco-PC
+"""
+import networkx as nx
+import simulation as simx
+import SirModel as sir
+
+from utils import onShow
+
+alpha=0.999
+beta=0.999
+gamma=0.999
+delta=0.999
+
+G=nx.gnp_random_graph(100, 0.1)
+
+model=sir.SirModel(G,alpha,beta)
+sim=simx.Simulator(model.initialState(),
+                   model.transition(),
+                   onShow)
+sim.run(1000,5)
+
+
+
+
+
--- NS/utils.py
+++ NS/utils.py
@@ -1,21 +1,12 @@
 # -*- coding: utf-8 -*-
 """
-Created on Wed Nov  2 15:44:05 2022
+Created on Wed Nov  9 12:23:14 2022
 
 @author: Marco-PC
 """
-import networkx as nx
-import simulation as simx
-import SirModel as sir
-import SisModel as sis
-import SirsModel as sirs
-import SeirdsModel as seirds
-import matplotlib.pyplot as plt
 
-alpha=0.999
-beta=0.999
-gamma=0.999
-delta=0.999
+import networkx as nx
+import matplotlib.pyplot as plt
 
 def color(num,state):
     ret=None
@@ -42,16 +33,3 @@ def onShow(G,state):
     nx.draw(G,with_labels=True,
             node_color=colorList)
     plt.show()
-
-G=nx.gnp_random_graph(100, 0.23)
-
-model=sir.SirModel(G,alpha,beta)
-sim=simx.Simulator(model.initialState(),
-                   model.transition(),
-                   onShow)
-sim.run(1000,5)
-
-
-
-
-
